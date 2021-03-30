8b3592644a0bd152cbcec1a7b15fd68a00dc167e
eoloritzadam1 <eoloritzadam1@gmail.com>
2021-03-30 16:57:52 +0200

    Añadido comentario a la clase PruebaGitEclipse

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -2,6 +2,11 @@ package prueba.test;
 
 import prueba.modelo.Saludo;
 
+/**
+ * Punto de entrada a la aplicación
+ * @author Elorri
+ *
+ */
 public class PruebaGitEclipse {
 
 	public static void main(String[] args) {
